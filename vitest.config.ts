76803de9766a140463b260bed3b5a457f9faded6
shiyuan <1213423761@qq.com>
2026-07-01 12:01:08 +0800

    build(project): restore git hook toolchain

--- vitest.config.ts
+++ vitest.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
     },
     exclude: [
       ...configDefaults.exclude,
+      '**/.codex/**',
       '**/e2e/**',
       '**/dist/**',
       '**/.{idea,git,cache,output,temp}/**',
